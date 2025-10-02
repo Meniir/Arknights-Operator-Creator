9ab28b7e690f43765b7e887c41c368f677a67045
Meniir <137597972+Meniir@users.noreply.github.com>
2025-10-02 07:44:40 +0700

    Add files via upload

---
+++ changelog.js
@@ -0,0 +1,14 @@
+const changelogData = [
+     {
+        version: '1.1 (2/10/2025)',
+        changes: [
+            'Added a changelog.',
+            'Expanded the attack range grid from 7x7 to 9x9.',
+            'Added "Earthling" race.',
+            'Added ASPD as selectable attribute for module bonuses.',
+            'Added a "Set Skill Attack Range" button for each skill.',
+            'Hovering over a skill in the Preview panel now shows its unique attack range.',
+            'The Base attack range in the Preview is now highlighted in orange.'
+        ]
+    }
+];
